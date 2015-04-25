8282a07b52c61fa03396343565e7a5c65341a5c8
Thomas VANACKER <thomas.vanacker@bbox.fr>
2015-04-25 11:10:31 +0200

    Listview gere les click EditView generee

--- app/src/main/java/com/example/thomas/todolist/MainActivity.java
+++ app/src/main/java/com/example/thomas/todolist/MainActivity.java
@@ -29,14 +29,16 @@ public class MainActivity extends ActionBarActivity {
     private ArrayList<HashMap<String, Object>> items;
     private SimpleAdapter itemsAdapter;
     private ListView lvItems;
+
     private int positionOnClick;
     private ListView editLvItems;
+    private ArrayList<HashMap<String, Object>> subItems;
+    private SimpleAdapter editItemsAdapter;
 
 
     public void populateSetDate(int year, int month, int day) {
-        mEdit = (TextView) findViewById(R.id.date);
-        String machin = String.format("%02d/%02d/%04d", day, month, year);
-        mEdit.setText(machin);
+        mEdit = (TextView) findViewById(R.id.itemDate);
+        mEdit.setText(String.format("%02d/%02d/%04d", day, month, year));
     }
 
 
@@ -97,6 +99,15 @@ public class MainActivity extends ActionBarActivity {
 
     private void displayEditTodoListView() {
         setContentView(R.layout.edittodolayout);
+        editLvItems = (ListView) findViewById(R.id.editItemListView);
+        subItems = new ArrayList<>();
+        editItemsAdapter = new SimpleAdapter(this,
+                subItems,
+                R.layout.sub_task_layout,
+                new String[]{"isDone", "title"},
+                new int[]{R.id.subTaskIsDone, R.id.subTaskTitle});
+        editLvItems.setAdapter(editItemsAdapter);
+        setupListViewListener();
     }
 
     public void onTodoItemFinished(View view) {
@@ -128,20 +139,61 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 positionOnClick = position;
-                Object listItem = lvItems.getItemAtPosition(positionOnClick);
-
+                HashMap<String, Object> listItem = items.get(positionOnClick);
+                displayEditTodoListView();
+                populateEditView(listItem);
             }
         });
+    }
+
+    private void populateEditView(HashMap<String, Object> item) {
+        EditText mEditTitle = (EditText) findViewById(R.id.editItemTitle);
+        mEditTitle.setText((String) item.get("title"));
+        EditText mEditCategory = (EditText) findViewById(R.id.editItemCategory);
+        mEditCategory.setText((String) item.get("category"));
+        EditText mEditComment = (EditText) findViewById(R.id.editItemComment);
+        mEditComment.setText((String) item.get("comment"));
+        TextView mEditDate = (TextView) findViewById(R.id.editItemDate);
+        mEditDate.setText((String) item.get("date"));
+        TextView mEditPeriodicity = (TextView) findViewById(R.id.editItemPeriodicity);
+        mEditPeriodicity.setText((String) item.get("periodicity"));
+
+        subItems = (ArrayList<HashMap<String, Object>>) item.get("subTaskList");
+        editItemsAdapter.notifyDataSetChanged();
+
+
+    }
+
+    public void onAddSubItem(View v) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("isDone", false);
+        map.put("title", ((EditText) findViewById(R.id.addSubTaskTitle)).getText());
+        subItems.add(map);
+    }
+
+    public void onEditItem(View v) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("isDone", false);
+        map.put("title", ((EditText) findViewById(R.id.editItemTitle)).getText());
+        map.put("comment", ((EditText) findViewById(R.id.editItemComment)).getText());
+        map.put("category", ((EditText) findViewById(R.id.editItemCategory)).getText());
+        map.put("date", ((TextView) findViewById(R.id.editItemDate)).getText());
+        map.put("periodicity", ((TextView) findViewById(R.id.editItemPeriodicity)).getText());
+        map.put("subTaskList", subItems);
+        items.remove(positionOnClick);
+        items.add(positionOnClick, map);
+
     }
 
     public void onAddItem(View v) {
         HashMap<String, Object> map = new HashMap<>();
         map.put("isDone", false);
-        map.put("title", ((EditText) findViewById(R.id.newItemTitle)).getText());
-        map.put("comment", ((EditText) findViewById(R.id.newItemComment)).getText());
-        map.put("category", ((EditText) findViewById(R.id.newItemCategory)).getText());
-        map.put("date", ((TextView) findViewById(R.id.date)).getText());
-        map.put("periodicity", ((TextView) findViewById(R.id.periodicity)).getText());
+        map.put("title", ((EditText) findViewById(R.id.itemTitle)).getText());
+        map.put("comment", ((EditText) findViewById(R.id.itemComment)).getText());
+        map.put("category", ((EditText) findViewById(R.id.itemCategory)).getText());
+        map.put("date", ((TextView) findViewById(R.id.itemDate)).getText());
+        map.put("periodicity", ((TextView) findViewById(R.id.itemPeriodicity)).getText());
+        map.put("subTaskList", new ArrayList<HashMap<String, Object>>());
         items.add(map);
         writeItems();
         // Retourne à la liste des items
@@ -167,8 +219,8 @@ public class MainActivity extends ActionBarActivity {
             case R.id.menu_addItem:
                 setContentView(R.layout.layout_add_item);
                 String[] spinnerList = {"Canada", "Mexico", "USA"};
-                createSpinner(spinnerList, R.id.newItemCategory, R.id.spinnerCategory);
-                createSpinner(spinnerList, R.id.periodicity, R.id.spinnerPeriodicity);
+                createSpinner(spinnerList, R.id.itemCategory, R.id.spinnerCategory);
+                createSpinner(spinnerList, R.id.itemPeriodicity, R.id.spinnerPeriodicity);
                 return true;
             case R.id.menu_goToList:
                 displayTodoListView();
