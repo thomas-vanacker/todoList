0f03593f878c08711f9d341ddd2148e5c60b55c0
Thomas VANACKER <thomas.vanacker@bbox.fr>
2015-05-10 18:16:36 +0200

    Modif Gradle

---
+++ app/src/main/java/com/example/thomas/todolist/Map.java
@@ -0,0 +1,11 @@
+package com.example.thomas.todolist;
+
+import android.support.v4.app.FragmentActivity;
+
+
+/**
+ * Created by Thomas on 10/05/2015.
+ */
+public class Map extends FragmentActivity {
+
+}
